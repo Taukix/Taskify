89ddd8b8c1e01b729d15495111e05b1f97b57c50
Tommy BOISSEAU <tommy.boisseau@etu.unilim.fr>
2023-03-07 21:58:45 +0100

    Ajout du design de la page pour ajouter des tâches

--- app/src/main/java/com/example/todo_list_project/MainActivity.kt
+++ app/src/main/java/com/example/todo_list_project/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.todo_list_project
 
 import TaskAdapter
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -14,18 +19,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var taskAdapter: TaskAdapter
     private lateinit var taskList: List<Task>
 
+    private var startingDateButton: Button = findViewById(R.id.buttonStartingDate)
+    private var endingDateButton: Button = findViewById(R.id.buttonEndingDate)
+    private var reminderDateButton: Button = findViewById(R.id.buttonReminderDate)
+    private var reminderTimeButton: Button = findViewById(R.id.buttonReminderTime)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Initialisez votre liste de tâches ici...
+        // Initialise notre liste de tâches
         taskList = getTaskList()
 
-        // Initialisez votre RecyclerView ici...
+        // Initialise notre RecyclerView
         val recyclerView: RecyclerView = findViewById(R.id.listOfTask)
         recyclerView.layoutManager = LinearLayoutManager(this)
         taskAdapter = TaskAdapter(taskList)
         recyclerView.adapter = taskAdapter
+
+        // Initialise nos dates
+        initDatePicker()
+        initTimePicker()
+        startingDateButton.setText(getTodaysDate())
+        endingDateButton.setText(getTodaysDate())
+        reminderDateButton.setText(getTodaysDate())
+        reminderTimeButton.setText(getTime())
     }
 
     private fun getTaskList(): List<Task> {
@@ -64,5 +82,49 @@ class MainActivity : AppCompatActivity() {
         )
 
         return taskList
+    }
+
+    fun getTodaysDate(): String {
+        val date = Calendar.getInstance().time
+        return makeDateString(date.day, date.month, date.year)
+    }
+
+    fun initDatePicker() {
+        DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            val date = Calendar.getInstance()
+            date.set(Calendar.YEAR, year)
+            date.set(Calendar.MONTH, month)
+            date.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            startingDateButton.text = dateFormat.format(date.time)
+            endingDateButton.text = dateFormat.format(date.time)
+            reminderDateButton.text = dateFormat.format(date.time)
+        }
+
+    }
+
+    fun makeDateString(day: Int, month: Int, year: Int): String {
+        return "$day/$month/$year"
+    }
+
+    fun initTimePicker() {
+        TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+            val date = Calendar.getInstance()
+            date.set(Calendar.HOUR_OF_DAY, hourOfDay)
+            date.set(Calendar.MINUTE, minute)
+
+            val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+            reminderTimeButton.text = dateFormat.format(date.time)
+        }
+    }
+
+    fun makeTimeString(hour: Int, minute: Int): String {
+        return "$hour:$minute"
+    }
+
+    fun getTime(): String {
+        val date = Calendar.getInstance().time
+        return makeTimeString(date.hours, date.minutes)
     }
 }
