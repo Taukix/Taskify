f50619d29605fca91df195a3188a0db3a12e7ff7
Tommy BOISSEAU <tommy.boisseau@etu.unilim.fr>
2023-03-07 22:33:19 +0100

    Ajout SplashActivity

--- app/src/main/java/com/example/todo_list_project/MainActivity.kt
+++ app/src/main/java/com/example/todo_list_project/MainActivity.kt
@@ -19,11 +19,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var taskAdapter: TaskAdapter
     private lateinit var taskList: List<Task>
 
-    private var startingDateButton: Button = findViewById(R.id.buttonStartingDate)
-    private var endingDateButton: Button = findViewById(R.id.buttonEndingDate)
-    private var reminderDateButton: Button = findViewById(R.id.buttonReminderDate)
-    private var reminderTimeButton: Button = findViewById(R.id.buttonReminderTime)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -37,14 +32,6 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         taskAdapter = TaskAdapter(taskList)
         recyclerView.adapter = taskAdapter
-
-        // Initialise nos dates
-        initDatePicker()
-        initTimePicker()
-        startingDateButton.setText(getTodaysDate())
-        endingDateButton.setText(getTodaysDate())
-        reminderDateButton.setText(getTodaysDate())
-        reminderTimeButton.setText(getTime())
     }
 
     // Gestion de la liste de Tâches
@@ -87,52 +74,5 @@ class MainActivity : AppCompatActivity() {
         return taskList
     }
 
-    //-------------------------------------------------------------------------------------
-
-    // Gestion des dates et heures pour l'ajout des tâches
-    fun getTodaysDate(): String {
-        val date = Calendar.getInstance().time
-        return makeDateString(date.day, date.month, date.year)
-    }
-
-    fun initDatePicker() {
-        DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-            val date = Calendar.getInstance()
-            date.set(Calendar.YEAR, year)
-            date.set(Calendar.MONTH, month)
-            date.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-
-            val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
-            startingDateButton.text = dateFormat.format(date.time)
-            endingDateButton.text = dateFormat.format(date.time)
-            reminderDateButton.text = dateFormat.format(date.time)
-        }
-
-    }
-
-    fun makeDateString(day: Int, month: Int, year: Int): String {
-        return "$day/$month/$year"
-    }
-
-    fun initTimePicker() {
-        TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
-            val date = Calendar.getInstance()
-            date.set(Calendar.HOUR_OF_DAY, hourOfDay)
-            date.set(Calendar.MINUTE, minute)
-
-            val dateFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
-            reminderTimeButton.text = dateFormat.format(date.time)
-        }
-    }
-
-    fun makeTimeString(hour: Int, minute: Int): String {
-        return "$hour:$minute"
-    }
-
-    fun getTime(): String {
-        val date = Calendar.getInstance().time
-        return makeTimeString(date.hours, date.minutes)
-    }
-
-    //--------------------------------------------------------------------------------
+    //----------------------------------------------------------------------------------------------
 }
